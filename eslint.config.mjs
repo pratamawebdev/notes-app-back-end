8dbd1c42a580394dfddd44494d2c2a1d7f1f73fb
willy <willypratama231@gmail.com>
2024-06-07 20:25:27 +0800

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
+  {
+    rules: {
+      "no-console": "off",
+    },
+  },
+  pluginJs.configs.recommended,
+];
